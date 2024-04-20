102b9773fafdd9f91864b15dbb0c0e12954bf2f7
februar-END <nobuhiro.0320@gmail.com>
2024-04-20 15:04:24 +0000

    url_test

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useData } from '../contexts/DataContext';
 
 const StartButton: React.FC = () => {
     const { setResponse, setStatusCode } = useData();
 
+    useEffect(() => {
+        axios.defaults.withCredentials = true;
+    }, []);
+
     const handleStart = async () => {
         try {
             const requestBody = {
-                image_path: 'https://lambda.com',
-                success: true,
-                message: '失敗しました',
-                class_name: 1,
-                confidence: '0.0004',
-                request_timestamp: '2024-04-22T00:00:00+09:00',
-                response_timestamp: '2024-04-23T12:00:00+09:00'
+                image_path: '/image/aaaa/bbbb/test.gisdff',
             };
 
-            const response = await axios.post('http://127.0.0.1:8000/ai_analysis_logs/create/', requestBody);
+            const response = await axios.post('https://7qpvy8fkel.execute-api.ap-northeast-1.amazonaws.com/dev/ai_analysis_log', requestBody);
 
             setResponse(JSON.stringify(response.data, null, 2));
             setStatusCode(response.status);
         } catch (error: any) {
-            setResponse(error.message);
+            setResponse(JSON.stringify(error.response?.data, null, 2));
             setStatusCode(error.response?.status || null);
         }
     };
