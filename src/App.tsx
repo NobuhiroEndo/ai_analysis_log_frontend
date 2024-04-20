fef4c202790007a8c451608e48a906464bec3655
februar-END <nobuhiro.0320@gmail.com>
2024-04-20 11:14:16 +0000

    frontend first commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+
+const App: React.FC = () => {
+  const [response, setResponse] = useState<string>('');
+
+  const handleStart = async () => {
+    try {
+      const requestBody = {
+        image_path: 'https://lambda.com',
+        success: true,
+        message: '失敗しました',
+        class_name: 1,
+        confidence: '0.0004',
+        request_timestamp: '2024-04-22T00:00:00+09:00',
+        response_timestamp: '2024-04-23T12:00:00+09:00'
+      };
+
+      const response = await axios.post('http://127.0.0.1:8000/ai_analysis_logs/list/', requestBody);
+      setResponse(JSON.stringify(response.data, null, 2));
+    } catch (error: any) {
+      setResponse(error.message);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="App-logo" alt="logo" />
+        <img src={viteLogo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        
+        <button className="App-link" onClick={handleStart}>
+          開始！
+        </button>
+        {response && <pre>{response}</pre>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
